f4378b05411532a133138569724cfeac967dda6d
EthanCarson <digitalec414@outlook.com>
2025-03-19 16:46:25 -0400

    Not WOrking

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import styles from './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  function managePratice(e){
+    e.preventDefault();
+    //Collect the values from the form
+    let praticeHours = document.getElementById('praticeHours').value;
+    let totalTime = document.getElementById('totalTime').value;
+    let timeUnit = document.getElementById('timeUnit').value;
+    let repeat = document.getElementById('repeat').checked;
+   // console.log(praticeHours, totalTime, timeUnit, repeat);
+//Get current Date
+const startDate = new Date();
+//Convert totalTime to the unit of the timeUnit
+if(timeUnit === 'hours'){
+      totalTime = totalTime * 1000 * 60 * 60; //Convert to milliseconds
+    }else if(timeUnit === 'days'){
+      totalTime = totalTime * 1000 * 60 * 60 * 24; //Convert to milliseconds
+    }else if(timeUnit === 'weeks'){
+      totalTime = totalTime * 1000 * 60 * 60 * 24 * 7; //Convert to milliseconds
+    }
+console.log(startDate.getTime());
+console.log(totalTime);
+const endDate = (timeUnit = 'months') ? startDate.getMonth() + +totalTime : startDate.getTime() + totalTime
+console.log(startDate, endDate);
+//TODO: Add the values to the Log
+  }
+
+
   return (
     <div class={styles.App}>
 
       {/*Form to input the number of hours to practice and the total time to practice*/}
 
-    <form>
+    <form onsubmit={managePratice}>
 
       <input type="number" id='praticeHours'/>
       <input type="number" id='totalTime'/>
