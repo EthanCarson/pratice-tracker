e6130e7d42a5f2ed6e94b0fbba0ee0254a854069
EthanCarson <digitalec414@outlook.com>
2025-03-19 16:08:23 -0400

    Intilized Repository

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import styles from './App.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div class={styles.App}>
+
+      {/*Form to input the number of hours to practice and the total time to practice*/}
+    <form>
+      <input type="number" id='praticeHours'/>
+      <input type="number" id='totalTime'/>
+      <select name="timeUnit" id="timeUnit">
+        <option value="days">day(s)</option>
+        <option value="weeks">week(s)</option>
+        <option value="months">month(s)</option>
+      </select>
+      <input type="checkbox" name="repeat" id="repeat" />
+      <input type="submit" />
+    </form>
+{/*Button to start the timer*/}
+    <button>Start</button>
+    <div id="Timer"></div>
+{/*Log of the time spent practicing*/}
+    <table id="Log">
+      <tr>
+        <th>Date</th>
+        <th>Start</th>
+        <th>End</th>
+      </tr>
+    </table>
+    </div>
+  );
+}
+
+export default App;
