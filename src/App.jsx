9daaaf40d17f51e6b2512fd98fe27775b549a8a1
EthanCarson <digitalec414@outlook.com>
2025-03-19 16:11:51 -0400

    Framework Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,41 @@ function App() {
     <div class={styles.App}>
 
       {/*Form to input the number of hours to practice and the total time to practice*/}
+
     <form>
+
       <input type="number" id='praticeHours'/>
       <input type="number" id='totalTime'/>
+
       <select name="timeUnit" id="timeUnit">
         <option value="days">day(s)</option>
         <option value="weeks">week(s)</option>
         <option value="months">month(s)</option>
       </select>
+      
       <input type="checkbox" name="repeat" id="repeat" />
       <input type="submit" />
+
     </form>
+
 {/*Button to start the timer*/}
+
     <button>Start</button>
     <div id="Timer"></div>
+
 {/*Log of the time spent practicing*/}
+
     <table id="Log">
+<thead>
       <tr>
         <th>Date</th>
         <th>Start</th>
         <th>End</th>
       </tr>
+      </thead>
+      <tbody>
+      {/*Add Components here*/}
+</tbody>
     </table>
     </div>
   );
